24b9d9c2c8c238fa1f8a758d3a188c435f094757
Your Name <you@example.com>
2026-05-25 18:53:59 +0400

    Fix Netlify handler and update netlify.toml

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,8 @@
 const serverless = require('serverless-http');
-const app = require('../../server.js');
 
-module.exports.handler = serverless(app);
+// server.js { app, helpers } objesi export ediyor,
+// sadece Express app'ini alıyoruz
+const serverModule = require('../../server.js');
+const app = serverModule && serverModule.app ? serverModule.app : serverModule;
+
+exports.handler = serverless(app);
